2a2aa7fe893abf082168ebd8ece4dbbccb378c7b
Dharnish D P <dharnishdp@Dharnishs-MacBook-Pro.local>
2024-01-04 21:20:13 +0530

    updated

--- script/script.js
+++ script/script.js
@@ -2,50 +2,54 @@ const correctPassword = "bubududu";
 const enteredPassword = prompt("Please enter the password:");
 // enteredPassword === correctPassword
 if (enteredPassword === correctPassword) {
-    const audio = new Audio('audio/happy-birthday-jazz-171120.mp3');
-    var audio_flag=0;
-    const playAudio = () => {  
-        audio.play();
-      };
-      
-      const playButton = document.querySelector('.canvas');
-      playButton.addEventListener('click', () => {
-        if(audio_flag===0){
-            const candle = document.querySelector('.candle');
-            candle.classList.add('altered');
-            playAudio();
-            audio_flag=1;
-        }
-      });
+  const audio = new Audio('audio/happy-birthday-jazz-171120.mp3');
+  var audio_flag=0;
+  
+  const playAudio = () => {  
+      audio.play();
+    };
 
-    var colors = ['#C0392B', '#ECF0F1'];
-    setTimeout(() => {
-        confetti({
+  function blaster() {
+      var colors = ['#C0392B', '#ECF0F1'];
+      setTimeout(() => {
+          confetti({
           particleCount: 500,
           angle: 90,
           spread: 50,
           origin: { x: 0.1, y: 1.4 },
           colors: colors,
           startVelocity: 170
-        });
-        confetti({
+          });
+          confetti({
           particleCount: 500,
           angle: 90,
           spread: 50,
           origin: { x: 0.3, y: 1.4 },
           colors: colors,
           startVelocity: 170
-        });
-        confetti({
+          });
+          confetti({
           particleCount: 500,
           angle: 90,
           spread: 50,
           origin: { x: 0.5, y: 1.4 },
           colors: colors,
           startVelocity: 170
-        });
+          });
       }, 500);
-  } else {
-    document.body.innerHTML = "";
-    document.body.innerHTML = "<h1>Incorrect Password</h1><p>Please try again.</p>";
   }
+  blaster() 
+  const playButton = document.querySelector('.canvas');
+  playButton.addEventListener('click', () => {
+  if(audio_flag===0){
+      const candle = document.querySelector('.candle');
+      candle.classList.add('altered');
+      playAudio();
+      audio_flag=1;
+      blaster() 
+  }
+  });
+} else {
+  document.body.innerHTML = "";
+  document.body.innerHTML = "<h1>Incorrect Password</h1><p>Please try again.</p>";
+}
